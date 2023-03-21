78938595a16cf1176e6ded9df4b81300a3db4fcd
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-21 23:13:23 +0530

    day-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import About from './components/About'
+import Stars from './components/canvas/Stars'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import Feedbacks from './components/Feedbacks'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Tech from './components/Tech'
+import Works from './components/Works'
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover'></div>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className='relative z-0'>
+          <Contact/>
+          <Stars/>
+        </div>
       </div>
     </BrowserRouter>
   )
