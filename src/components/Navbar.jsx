cc071bfc56336dabe968ec4b5ff421d533680e72
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-22 17:54:29 +0530

    day-3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { styles } from '../styles'
 
 const Navbar = () => {
+  const [active, setActive] = useState('');
+
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className='w-full flex justify-between items-center'></div>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link to='/' className='flex items-center gap-2' onClick={()=>{setActive('');}}></Link>
+      </div>
     </nav>
   )
 }
