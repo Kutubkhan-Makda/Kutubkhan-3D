572353f605ec8f5bd64a4f79594bea6094e402d7
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-22 18:03:02 +0530

    day-3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,9 @@ const Navbar = () => {
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link to='/' className='flex items-center gap-2' onClick={()=>{setActive('');}}></Link>
+        <Link to='/' className='flex items-center gap-2' onClick={()=>{setActive('');window.scrollTo(0,0);}}>
+          <img src={logo} alt="logo" />
+        </Link>
       </div>
     </nav>
   )
