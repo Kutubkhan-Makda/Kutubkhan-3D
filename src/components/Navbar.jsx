619219f1daf75c4793708acd960de6eb6ba19ee7
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-22 22:03:21 +0530

    day-3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to='/' className='flex items-center gap-2' onClick={()=>{setActive('');window.scrollTo(0,0);}}>
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Kutubkhan <span>Makda</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Kutubkhan <span className='sm:block hidden'>Makda</span></p>
         </Link>
       </div>
     </nav>
