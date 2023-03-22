48c24abb13ab3d94253fee3b161566c01f4bb7d2
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-22 17:47:08 +0530

    day-3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { styles } from '../styles'
 
 const Navbar = () => {
   return (
-    <nav>
-      
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center'></div>
     </nav>
   )
 }
