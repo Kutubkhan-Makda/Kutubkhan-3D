ee857acf17cb9493aa84e61435dcb6844f537fcb
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-22 18:25:57 +0530

    day-3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { styles } from '../styles'
+import {logo} from '../assets/index'
 
 const Navbar = () => {
   const [active, setActive] = useState('');
@@ -9,7 +10,8 @@ const Navbar = () => {
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to='/' className='flex items-center gap-2' onClick={()=>{setActive('');window.scrollTo(0,0);}}>
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Kutubkhan <span>Makda</span></p>
         </Link>
       </div>
     </nav>
