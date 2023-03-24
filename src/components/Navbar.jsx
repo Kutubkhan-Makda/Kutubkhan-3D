e1a7924710757b59648db6b75f570cf797156efe
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-24 16:10:40 +0530

    day-5

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,14 @@ const Navbar = () => {
           ))}
         </ul>
         <div className='sm:hidden flex flex-1 justify-end items-center'>
-            <img src={menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={()=>setToggle(!toggle)} />
+            <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={()=>setToggle(!toggle)} />
+            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient adsolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {navLinks.map((link)=>(
+                <li key={link.id} className={`${active===link.title ? 'text-white' : 'text-secondary'} font-poppins font-medium cursor-pointer text-[16px]`} onClick={() => {setToggle(!toggle); setActive(link.title);}}><a href={`#${link.id}`}>{link.title}</a></li>
+              ))}
+            </ul>
+            </div>
         </div>
       </div>
     </nav>
