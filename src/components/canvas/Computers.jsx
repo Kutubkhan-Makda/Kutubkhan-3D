251baa5c7e0e0795c7cb24ed18accadf16b1dca0
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-25 23:15:10 +0530

    day-6

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,9 +3,17 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
 
 const Computers = () => {
+  const comuter = useGLTF('./desktop_pc/scene.gltf');
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pointLight intensity={1}/>
+      <primitive object={comuter.scene}/>
+    </mesh>
   )
 }
+
+const computerCanvas = () => {}
 
 export default Computers
