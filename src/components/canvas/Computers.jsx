6eff148661a471e9c857baca1076dfd08a6ce5c9
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-04-03 00:46:35 +0530

    day-8

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,14 +1,16 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
+import { HemisphereLight, PointLight } from 'three';
+import Loader from '../Loader';
 
 const Computers = () => {
   const comuter = useGLTF('./desktop_pc/scene.gltf');
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <pointLight intensity={1}/>
+      <HemisphereLight intensity={0.15} groundColor='black' />
+      <PointLight intensity={1}/>
       <primitive object={comuter.scene}/>
     </mesh>
   )
@@ -17,9 +19,11 @@ const Computers = () => {
 const computerCanvas = () => {
   return (
     <Canvas frameloop='demand' shadows camera={{position:[20,3,5],fov:25}} gl={{preserveDrawingBuffer:true}}>
-      <Suspense fallback={<CanvasLoader/>}>
-        <OrbitControls enableZoom={false}/>
+      <Suspense fallback={<Loader/>}>
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2}/>
+        <Computers/>
       </Suspense>
+      <Preload all/>
     </Canvas>
   )
 }
