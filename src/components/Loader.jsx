cdd83d210c2d6aa1571c80c38729a7f42334358a
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-20 00:43:59 +0530

    day-9

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,14 @@
+import { Html, useProgress } from '@react-three/drei'
 import React from 'react'
 
 const Loader = () => {
+  const {progress} = useProgress();
+
   return (
-    <div>Loader</div>
+    <Html>
+      <span className='canvas-loader'></span>
+      <p style={{fontSize:14,color:'#F1F1F1',fontWeight:800,marginTop:40}}>{progress.toFixed(2)}%</p>
+    </Html>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,22 +1,22 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
-import { HemisphereLight, PointLight } from 'three';
 import Loader from '../Loader';
 
 const Computers = () => {
-  const comuter = useGLTF('./desktop_pc/scene.gltf');
+  const computer = useGLTF('./desktop_pc/scene.gltf');
 
   return (
     <mesh>
-      <HemisphereLight intensity={0.15} groundColor='black' />
-      <PointLight intensity={1}/>
-      <primitive object={comuter.scene}/>
+      <hemisphereLight intensity={0.2} groundColor='black' />
+      <pointLight intensity={0.8}/>
+      <spotLight position={[-20,50,10]} angle={0.12} penumbra={1} intensity={0.7} castShadow shadow-mapSize={1024}/>
+      <primitive object={computer.scene} scale={0.75} position={[0,-3.25,-1.5]} rotation={[-0.01,-0.2,-0.05]}/>
     </mesh>
   )
 }
 
-const computerCanvas = () => {
+const ComputersCanvas = () => {
   return (
     <Canvas frameloop='demand' shadows camera={{position:[20,3,5],fov:25}} gl={{preserveDrawingBuffer:true}}>
       <Suspense fallback={<Loader/>}>
