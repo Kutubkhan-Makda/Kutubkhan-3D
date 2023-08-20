0103a9e6cd2ef0dd085d6f36b5c978098eae0ae9
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-20 17:56:24 +0530

    day-9

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
 import Loader from '../Loader';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Computers = ({isMobile}) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
@@ -21,7 +22,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width:500px)');
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
     setIsMobile(mediaQuery.matches);
 
     const handleMediaQueryChange = (event) => {
