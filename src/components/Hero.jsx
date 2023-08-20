b068e969ac875801277c84627512f4b7ca3c69e3
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-20 17:34:59 +0530

    day-9

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 import { styles } from '../styles'
-import Computers from './canvas/Computers'
+import ComputersCanvas from './canvas/Computers'
 
 const Hero = () => {
   return (
@@ -18,7 +18,14 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <Computers/>
+      <ComputersCanvas/>
+      <div className='absolute xs:bottom-10 bottom-32 w-full justify-center flex items-center'>
+        <a href="#about">
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.dev animate={{y:[0,24,0]}} transition={{duration:1.5,repeat:Infinity,repeatType:'loop'}} className='w-3 h-3 rounded-full bg-secondary mb-1'/>
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
