f6df471f54d002fce360dff285d8d82c0d79ad7e
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-20 18:09:26 +0530

    day-9

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { fadeIn, textVariant } from '../Utils/motion'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.div>
+    <motion.p variants={fadeIn('','',0.1,1)}>
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+    </motion.p>
+    </>
   )
 }
 
