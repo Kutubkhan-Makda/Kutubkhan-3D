b3580a5a27dae1fc6c3e1eb7d49704967cc91864
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-21 19:32:27 +0530

    day-10

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -6,6 +6,9 @@ import { styles } from '../styles'
 const SectionWrapper = (Component,idName) => function HOC() {
     return(
         <motion.section variants={staggerContainer()} initial="hidden" whileInView="show" viewport={{once:true,amount:0.25}} className={`${styles.padding} max-w-7xl mx-auto relative z-0`}>
+            <span className='hash-span' id={idName}>
+                &nbsp;
+            </span>
             <Component/>
         </motion.section>
     )
