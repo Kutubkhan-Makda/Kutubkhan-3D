7c341696867943a79d92cfa81a2d170c387379c0
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-21 19:14:09 +0530

    day-10

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { staggerContainer } from '../Utils/motion'
+import { styles } from '../styles'
 
 const SectionWrapper = (Component,idName) => function HOC() {
     return(
-        <motion.section variants={staggerContainer()} initial="hidden" whileInView="show">
+        <motion.section variants={staggerContainer()} initial="hidden" whileInView="show" viewport={{once:true,amount:0.25}} className={`${styles.padding} max-w-7xl mx-auto relative z-0`}>
             <Component/>
         </motion.section>
     )
