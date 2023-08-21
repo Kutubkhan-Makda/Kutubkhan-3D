d864ed96dd4f9ee1e9861ecd1ac9862e4e692e19
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-21 17:56:04 +0530

    day-10

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const ServiceCard = ({index,title,icon}) => {
   return(
     <Tilt className='xs:w-[250px] w-full'>
       <motion.div variants={fadeIn("right","spring",0.5*index,0.75)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
-        <div options={{max:45,scale:1,speed:450}} className='bg-tertiary rounded-[20px] py-5 px-12'>
+        <div options={{max:45,scale:1,speed:450}} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
 
         </div>
       </motion.div>
