1e76c7df7113c9f1b66875e056c898b1b5d3d625
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-21 22:11:43 +0530

    day-10

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { textVariant } from '../Utils/motion'
+import SectionWrapper from '../hoc/SectionWrapper'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import { experiences } from '../Constants'
 
-const Experience = () => {
-  return (
-    <div>Experience</div>
+const ExperienceCard = ({experience}) => {
+  return(
+    <VerticalTimelineElement 
+    contentStyle={{background:'#1d1836',color:'#fff'}} 
+    contentArrowStyle={{borderRight:'7px solid #232631'}} 
+    date={experience.date} 
+    iconStyle={{background:experience.iconBg}} 
+    icon={<div>
+      <img src={experience.icon} alt={experience.company_name} className='w-[60%] h-[60%] object-contain' />
+    </div>}>
+      <div>
+        <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+      </div>
+    </VerticalTimelineElement>
   )
 }
 
-export default Experience
+const Experience = () => {
+  return (
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>What I have done so far</p>
+      <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+    </motion.div>
+    <div className='mt-20 flex flex-col'>
+      <VerticalTimeline>
+        {experiences.map((experience,index)=>(
+          <ExperienceCard experience={experience}/>
+        ))}
+      </VerticalTimeline>
+    </div>
+    </>
+  )
+}
+
+export default SectionWrapper(Experience,"experience") 
