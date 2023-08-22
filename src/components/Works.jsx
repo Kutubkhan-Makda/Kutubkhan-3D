1241e18195c97537e2f67edfb654a306fa2c5902
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-22 13:15:24 +0530

    day-11

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import { github } from '../assets'
 import SectionWrapper from '../hoc/SectionWrapper'
-import { textVariant } from '../Utils/motion'
+import { fadeIn, textVariant } from '../Utils/motion'
 import { styles } from '../styles'
 
 const Works = () => {
@@ -13,7 +13,13 @@ const Works = () => {
       <h2 className={styles.sectionHeadText}>Projects.</h2>
     </motion.div>
     <div className='w-full flex'>
-
+      <motion.p variants={fadeIn("","",0.1,1)} className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+        Following projects showcases my skills and experience through
+        real-world examples of my work. Each project is briefly described with
+        links to code repositories and live demos in it. It reflects my
+        ability to solve complex problems, work with different technologies,
+        and manage projects effectively.
+      </motion.p>
     </div>
     </>
   )
