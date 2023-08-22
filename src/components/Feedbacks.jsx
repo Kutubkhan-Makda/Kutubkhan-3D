08b01933a58e852f6c2f0f7b0a3726e5f9ef6f1a
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-22 22:52:23 +0530

    day-11

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -8,7 +8,21 @@ import { testimonials } from '../Constants'
 const FeedbackCard = ({index,testimonial,name,designation,company,image}) => {
   return(
     <motion.div variants={fadeIn("","spring",index*0.5,0.75)} className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'>
-      <p>{testimonial}</p>
+      <p className='text-white font-black text-[48px]'>"</p>
+      <div className='mt-1'>
+        <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+        <div className='mt-7 flex justify-between items-center gap-1'>
+          <div className='flex-1 flex flex-col'>
+            <p className='text-white text-[16px] font-medium'>
+              <span className='blue-text-gradient'>@</span> {name}
+            </p>
+            <p className='mt-1 text-secondary text-[12px]'>
+              {designation} of {company}
+            </p>
+          </div>
+          <img src={image} alt={`feedback-by-${name}`} className='w-10 h-10 rounded-full object-cover' />
+        </div>
+      </div>
     </motion.div>
   )
 }
@@ -19,7 +33,7 @@ const Feedbacks = () => {
       <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
         <motion.div variants={textVariant()}>
           <p className={styles.sectionSubText}>What others say</p>
-          <h2 className={styles.sectionHeadText}>Testimonails.</h2>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
       </div>
       <div className={`${styles.paddingX} mt-20 pb-14 flex flex-wrap gap-7`}>
