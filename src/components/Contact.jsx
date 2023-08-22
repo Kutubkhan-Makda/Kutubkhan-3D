ce3fde0105c8a8521517fe2491587c3168a4a2ec
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-22 23:06:50 +0530

    day-11

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { motion } from 'framer-motion'
+import SectionWrapper from '../hoc/SectionWrapper'
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({name:'',email:'',message:''})
+  const [loading, setLoading] = useState(false)
+
   return (
     <div>Contact</div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact,"contact") 
