b19001be5a97367f669a97291eada5ed382bd04c
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-23 00:02:43 +0530

    day-12

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,10 +1,15 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useRef } from 'react'
 import * as random from "maath/random/dist/maath-random.esm";
 import { Canvas } from '@react-three/fiber';
+import { Preload } from '@react-three/drei';
 
 const Stars = (props) => {
+  const ref = useRef()
+
   return (
-    <div>Stars</div>
+    <group>
+      
+    </group>
   )
 }
 
@@ -12,7 +17,10 @@ const StarsCanvas = () => {
   return (
     <div className='w-full h-full z-[-1] absolute inset-0'>
       <Canvas camera={{position:[0,0,1]}}>
-        <Suspense fallback={null}/>
+        <Suspense fallback={null}>
+          <Stars/>
+        </Suspense>
+        <Preload all/>
       </Canvas>
     </div>
   )
