4c0e6174adcc30343064cd51b640513b1a3e4ad1
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-22 12:00:06 +0530

    day-11

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,7 +8,7 @@ const Tech = () => {
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technologie)=>(
         <div className='w-28 h-28' key={technologie.name}>
-          <Ball/>
+          <Ball icon={technologie.icon}/>
         </div>
       ))}
     </div>
