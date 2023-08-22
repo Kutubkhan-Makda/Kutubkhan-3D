6c1193d32bc357d76cc601737b2527e71da8be25
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-22 12:39:25 +0530

    day-11

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -12,9 +12,9 @@ const Ball = (props) => {
       <ambientLight intensity={0.25}/>
       <directionalLight position={[0,0,0.05]}/>
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1,1]}/>
+        <icosahedronGeometry args={[1,5]} />
         <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading/>
-        <Decal position={[0,0,1]} map={decal}/>
+        <Decal position={[0,0,1]} rotation={[2*Math.PI,0,6.25]} map={decal} flatShading scale={1}/>
       </mesh>
     </Float>
   )
