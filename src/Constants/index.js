c8b1992cdc160f7518344ce5c1f375b3cdc38973
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-08-23 16:00:57 +0530

    day-12

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -203,7 +203,7 @@ import {
   
   const projects = [
     {
-      name: "Car Rent",
+      name: "Netfilm",
       description:
         "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
       tags: [
